ccf6962a3eda8819a14e8ee18d77e7babbc7e0aa
prudhvi <prudhvieethakota123@gmail.com>
2021-04-16 17:33:33 +0530

    Added:Read The Results from Dictionary into the Array

--- arithmetic_Computation-sorting.sh
+++ arithmetic_Computation-sorting.sh
@@ -9,18 +9,18 @@ echo "thirdInput:c"
 read a b c
 
 solution1=`echo "$a+$b*$c" | bc`
-echo $solution1
+echo ${!solution1[@]}
 
 solution2=`echo "$a*$b+$c" | bc`
-echo $solution2
+echo ${!solution2[@]}
 
 solution3=`echo "scale=2;$c+$a/$b" | bc`
-echo $solution3
+echo ${!solution3[@]}
 
 solution4=`echo "$a%$b+$c" | bc`
-echo $solution4
-
-echo ${!solution1[@]}
-echo ${!solution2[@]}
-echo ${!solution3[@]}
 echo ${!solution4[@]}
+
+echo ${solution1[@]}
+echo ${solution2[@]}
+echo ${solution3[@]}
+echo ${solution4[@]}
